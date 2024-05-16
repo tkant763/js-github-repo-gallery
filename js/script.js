8d73029c3cf64f4a2f8fc6eaef4b89e54460301a
tiffany_k <tiffany.kantz@gmail.com>
2024-05-15 20:10:43 -0400

    Wrote function fetching API data

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@
 const overviewDivElement = document.querySelector(".overview");
 // github username;
 const username = "tkant763";
+
+// fetch JSON data
+const getAPIData = async function() {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+    console.log(data);
+};
+
+getAPIData();
